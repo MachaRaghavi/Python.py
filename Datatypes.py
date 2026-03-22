ab73a8d9480b7f1611462691769a3137cefb6e52
MachaRaghavi <raghavimacha14@gmail.com>
2026-03-22 20:36:50 +0530

    Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,10 @@
+#Data types
+x=1
+y=2.0
+z="Raghavi"
+a=2+4j
+lst=["Apple","Banana","Grapes"]
+b=("Tomato","Potato")
+c={"name":"Raghavi","Age":19}
+print(type(x),type(y),type(z))
+print(type(a),type(lst),type(b),type(c))
